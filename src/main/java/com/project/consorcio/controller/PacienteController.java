67c961dbae75fed22e61e3d61f9ad08c85b27ae0
abellezap1980 <patbellezap@gmail.com>
2023-09-18 22:11:16 -0500

    Final clase 5

--- src/main/java/com/project/consorcio/controller/PacienteController.java
+++ src/main/java/com/project/consorcio/controller/PacienteController.java
@@ -1,10 +1,16 @@
 package com.project.consorcio.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.project.consorcio.entity.Categoria;
+import com.project.consorcio.entity.Paciente;
 import com.project.consorcio.service.CategoriaServices;
 import com.project.consorcio.service.PacienteServices;
 
@@ -22,7 +28,38 @@ public class PacienteController {
 		model.addAttribute("categorias",servicioCate.listarTodos());
 		return "paciente";
 	}
-	
+	//ruta o direccion URL para invocar al método grabar
+		@RequestMapping("/grabar")
+		public String grabar(@RequestParam("codigo") Integer codi,
+							 @RequestParam("nombre") String nom,
+							 @RequestParam("apellido") String ape,
+							 @RequestParam("sexo") String sexo,
+							 @RequestParam("categoria") int codCate,
+							 RedirectAttributes redirect) {		
+			try {
+				Paciente bean=new Paciente();
+				//setear atributos del objeto "med" usando los parámetros
+				bean.setNombre(nom);
+				bean.setApellido(ape);
+				bean.setSexo(sexo);
+				Categoria cate=new Categoria();
+				cate.setCodigo(codCate);
+				bean.setCategoria(cate);
+				if(codi==0) {
+					servicioPaci.registrar(bean);
+					redirect.addFlashAttribute("MENSAJE","Paciente registrado");
+				}
+				else {
+					bean.setCodigo(codi);
+					servicioPaci.actualizar(bean);
+					redirect.addFlashAttribute("MENSAJE","Paciente actualizado");
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return "redirect:/paciente/lista";
+		}
 	
 }
 
